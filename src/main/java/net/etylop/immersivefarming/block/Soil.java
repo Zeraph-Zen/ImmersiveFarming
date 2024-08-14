b31dabc0b60b5bede83cae169cdf7f2edeff830d
Etyl <etyl.sani@gmail.com>
2024-08-14 14:53:48 +0200

    feat: soil tilling

--- src/main/java/net/etylop/immersivefarming/block/Soil.java
+++ src/main/java/net/etylop/immersivefarming/block/Soil.java
@@ -2,7 +2,13 @@ package net.etylop.immersivefarming.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FarmBlock;
@@ -10,24 +16,52 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.extensions.IForgeBlock;
 import net.minecraftforge.common.extensions.IForgeBlockState;
+import org.lwjgl.system.CallbackI;
 
 public class Soil extends FarmBlock {
-    //public static final BooleanProperty DAMP = BooleanProperty.create("damp");
+    public static final int TILL_MAX = 7;
+    public static final IntegerProperty TILL = IntegerProperty.create("till",0,TILL_MAX);
 
     public Soil(Properties properties) {
         super(properties);
+        this.registerDefaultState(
+            this.stateDefinition.any()
+                .setValue(TILL, 0)
+        );
     }
 
     @Override
     public boolean canSustainPlant(BlockState state, BlockGetter level, BlockPos pos, Direction facing, IPlantable plantable) {
-        BlockState farmlandState = Blocks.FARMLAND.defaultBlockState();
-        return farmlandState.canSustainPlant(level, pos, facing, plantable);
+        if (state.hasProperty(Soil.TILL) && state.getValue(Soil.TILL) == Soil.TILL_MAX) {
+            BlockState farmlandState = Blocks.FARMLAND.defaultBlockState();
+            return farmlandState.canSustainPlant(level, pos, facing, plantable);
+        }
+        return false;
     }
 
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide() &&
+                pHand == InteractionHand.MAIN_HAND &&
+                pPlayer.getMainHandItem().getItem() instanceof HoeItem &&
+                pState.getValue(TILL) < TILL_MAX) {
+            int newTill = Math.min(TILL_MAX,pState.getValue(TILL)+1);
+            pLevel.setBlock(pPos, pState.setValue(TILL, newTill), 3);
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.FAIL;
+    }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(TILL);
+        super.createBlockStateDefinition(pBuilder);
+    }
 }
