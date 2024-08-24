c54ae96fd7abf52917ddd601fe815aa0ac9991f4
Etyl <etyl.sani@gmail.com>
2024-08-24 13:32:32 +0200

    sprinkler craft + item display

--- src/main/java/net/etylop/immersivefarming/block/IFBlocks.java
+++ src/main/java/net/etylop/immersivefarming/block/IFBlocks.java
@@ -29,7 +29,7 @@ public class IFBlocks {
             () -> new Soil(BlockBehaviour.Properties.copy(Blocks.FARMLAND))
     );
 
-    // TODO fix sprinkler occlusion
+    // TODO fix item block
     public static final RegistryObject<SprinklerBlock> SPRINKLER = registerBlock(
             "sprinkler",
             () -> new SprinklerBlock(Block.Properties.of(Material.METAL)
