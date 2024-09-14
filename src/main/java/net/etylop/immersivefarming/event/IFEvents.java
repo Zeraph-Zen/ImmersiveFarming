b565b788ae1779bcc3fe277a3ee9fccb0535dc0d
Etyl <etyl.sani@gmail.com>
2024-09-14 21:06:01 +0200

    changed UseHoeEvent

--- src/main/java/net/etylop/immersivefarming/event/IFEvents.java
+++ src/main/java/net/etylop/immersivefarming/event/IFEvents.java
@@ -6,7 +6,6 @@ import net.etylop.immersivefarming.block.custom.Soil;
 import net.etylop.immersivefarming.utils.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
-import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -16,7 +15,7 @@ import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.entity.player.BonemealEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,7 +24,6 @@ import net.minecraftforge.fml.common.Mod;
 import sereneseasons.api.season.Season;
 import sereneseasons.api.season.SeasonHelper;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,17 +34,33 @@ public class IFEvents {
         private static final Map<ChunkPos, List<BlockPos>> chunkCrops = new HashMap<>();
         private static final Map<BlockPos, Long> cropDate = new HashMap<>();
 
+        /*
         @SubscribeEvent
         public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
             Block target = event.getWorld().getBlockState(event.getPos()).getBlock();
             if (Registry.BLOCK.getHolderOrThrow(Registry.BLOCK.getResourceKey(target).get()).is(ModTags.Blocks.TILLABLE_BLOCK) &&
-                !event.getWorld().isClientSide() &&
-                event.getPlayer().getMainHandItem().getItem() instanceof HoeItem) {
+                    !event.getWorld().isClientSide() &&
+                    event.getPlayer().getMainHandItem().getItem() instanceof HoeItem) {
 
                 event.getWorld().setBlock(event.getPos(), IFBlocks.SOIL.get().defaultBlockState(), 3);
                 event.getPlayer().getMainHandItem().hurtAndBreak(1, event.getPlayer(), (val) -> {
                     val.broadcastBreakEvent(event.getPlayer().getUsedItemHand());
-                });            }
+                });
+            }
+        }*/
+
+        @SubscribeEvent
+        public static void onHoeUse(UseHoeEvent event) {
+            Level level = event.getContext().getLevel();
+            Block target = level.getBlockState(event.getContext().getClickedPos()).getBlock();
+            if (Registry.BLOCK.getHolderOrThrow(Registry.BLOCK.getResourceKey(target).get()).is(ModTags.Blocks.TILLABLE_BLOCK) &&
+                    !level.isClientSide()) {
+
+                level.setBlock(event.getContext().getClickedPos(), IFBlocks.SOIL.get().defaultBlockState(), 3);
+                event.getPlayer().getMainHandItem().hurtAndBreak(1, event.getPlayer(), (val) -> {
+                    val.broadcastBreakEvent(event.getPlayer().getUsedItemHand());
+                });
+            }
         }
 
         @SubscribeEvent
@@ -81,9 +95,9 @@ public class IFEvents {
 
             if (canCropGrow((Level) event.getWorld(), event.getPos())) {
                 int fertilization = event.getWorld().getBlockState(event.getPos().below()).getValue(Soil.FERTILITY);
-                if (fertilization==0 && Math.random()<0.75*0.5)
+                if (fertilization == 0 && Math.random() < 0.75 * 0.5)
                     event.setResult(Event.Result.DENY);
-                else if (fertilization==1 && Math.random()<0.5)
+                else if (fertilization == 1 && Math.random() < 0.5)
                     event.setResult(Event.Result.DENY);
                 else
                     event.setResult(Event.Result.DEFAULT);
@@ -102,19 +116,6 @@ public class IFEvents {
         }
 
         @SubscribeEvent
-        public static void onPlaceCrop(BlockEvent.EntityPlaceEvent event) {
-            if (event.getWorld().getBlockState(event.getPos()).getBlock() instanceof CropBlock &&
-            !event.getWorld().isClientSide()) {
-                cropDate.put(event.getPos(), event.getWorld().dayTime());
-                ChunkPos chunkPos = event.getWorld().getChunk(event.getPos()).getPos();
-                if (chunkCrops.get(chunkPos) == null) {
-                    chunkCrops.put(chunkPos, new ArrayList());
-                }
-                chunkCrops.get(chunkPos).add(event.getPos());
-            }
-        }
-
-        @SubscribeEvent
         public static void onHarvestCrop(BlockEvent.BreakEvent event) {
             BlockState block = event.getState();
             if (!(block.getBlock() instanceof CropBlock))
@@ -124,7 +125,7 @@ public class IFEvents {
             if (!(soil.getBlock() instanceof Soil))
                 return;
 
-            soil.setValue(Soil.FERTILITY,0);
+            soil.setValue(Soil.FERTILITY, 0);
         }
     }
 }
