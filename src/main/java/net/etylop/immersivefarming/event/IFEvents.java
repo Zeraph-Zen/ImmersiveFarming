b2cc5d52e7fce164b64d113a4ba7d5cd2fe5e1b4
Etyl <etyl.sani@gmail.com>
2024-11-06 00:27:24 +0000

    fix: default growth for non crop/stem

--- src/main/java/net/etylop/immersivefarming/event/IFEvents.java
+++ src/main/java/net/etylop/immersivefarming/event/IFEvents.java
@@ -14,7 +14,9 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.GrassBlock;
+import net.minecraft.world.level.block.StemBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -24,6 +26,7 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Collections;
 import java.util.Optional;
 
 
@@ -55,6 +58,13 @@ public class IFEvents {
             event.setCanceled(true);
         }
 
+
+        private static void handleCropGrowth(BlockEvent.CropGrowEvent.Pre event, int age, int maxAge) {
+            BlockState cropBlock = event.getState();
+            LevelAccessor level = event.getWorld();
+
+        }
+
         @SubscribeEvent(priority = EventPriority.LOW)
         public static void onCropGrowth(BlockEvent.CropGrowEvent.Pre event) {
             if (event.getResult() == Event.Result.DENY) return;
@@ -67,48 +77,51 @@ public class IFEvents {
                 return;
             }
 
+            if (!(cropBlock.getBlock() instanceof CropBlock || cropBlock.getBlock() instanceof StemBlock)) {
+                return;
+            }
+
             if (canCropGrow((Level) event.getWorld(), event.getPos())) {
-                BlockState soilBlock =  event.getWorld().getBlockState(event.getPos().below());
+                BlockState soilBlock = event.getWorld().getBlockState(event.getPos().below());
                 int fertilization = soilBlock.getValue(Soil.FERTILITY);
 
-                if (fertilization>=1) {
+                if (fertilization >= 1) {
                     event.setResult(Event.Result.DEFAULT);
-                }
-                else {
+                } else {
                     CropSavedData cropData = getCropSavedData((Level) event.getWorld());
-                    if (Math.random()<0.5 && cropData != null && cropData.testCrop((Level)level, event.getPos())) {
+                    if (Math.random() < 0.5 && cropData != null && cropData.testCrop((Level) level, event.getPos())) {
                         event.setResult(Event.Result.DEFAULT);
-                    }
-                    else if (Math.random()<0.25) {
+                    } else if (Math.random() < 0.25) {
                         event.setResult(Event.Result.DEFAULT);
-                    }
-                    else {
+                    } else {
                         event.setResult(Event.Result.DENY);
                     }
                 }
 
-                if (cropBlock.getBlock() instanceof CropBlock blockCropType) {
+                Optional<Integer> ageOptional = cropBlock.getValues().entrySet().stream()
+                        .filter(entry -> "age".equals(entry.getKey().getName()) && entry.getValue() instanceof Integer)
+                        .map(entry -> (Integer) entry.getValue())
+                        .findFirst();
 
-                    Optional<Integer> ageOptional = cropBlock.getValues().entrySet().stream()
-                            .filter(entry -> "age".equals(entry.getKey().getName()) && entry.getValue() instanceof Integer)
-                            .map(entry -> (Integer) entry.getValue())
-                            .findFirst();
+                Optional<Integer> maxAgeOptional = cropBlock.getValues().entrySet().stream()
+                        .filter(entry -> "age".equals(entry.getKey().getName()) && entry.getKey() instanceof IntegerProperty)
+                        .map(entry -> Collections.max(((IntegerProperty) entry.getKey()).getPossibleValues()))
+                        .findFirst();
 
-                    int age = ageOptional.orElse(0);
+                int age = ageOptional.orElse(0);
+                int maxAge= maxAgeOptional.orElse(10);
 
-                    if (age >= blockCropType.getMaxAge() - 1 && event.getResult() != Event.Result.DENY) {
-                        CropSavedData cropData = getCropSavedData((Level) event.getWorld());
-                        if (cropData != null) {
-                            cropData.insertCrop((Level) level, event.getPos());
-                            cropData.setDirty();
-                        }
-                        level.setBlock(event.getPos().below(), soilBlock.setValue(Soil.FERTILITY, 0), 2);
-                        event.setResult(Event.Result.ALLOW);
+                if (age >= maxAge - 1 && event.getResult() != Event.Result.DENY) {
+                    CropSavedData cropData = getCropSavedData((Level) event.getWorld());
+                    if (cropData != null) {
+                        cropData.insertCrop((Level) level, event.getPos());
+                        cropData.setDirty();
                     }
+                    level.setBlock(event.getPos().below(), soilBlock.setValue(Soil.FERTILITY, 0), 2);
+                    event.setResult(Event.Result.ALLOW);
                 }
-                return;
             }
-            event.setResult(Event.Result.DENY);
+
         }
 
         private static boolean canCropGrow(Level level, BlockPos pos) {
